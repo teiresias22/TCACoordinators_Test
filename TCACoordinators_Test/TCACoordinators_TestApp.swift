1a61479868dce658ff6bd7b7d6e0608c52541b38
teiresias22 <teiresias22@gmail.com>
2022-11-23 16:19:17 +0900

    Initial Commit

---
+++ TCACoordinators_Test/TCACoordinators_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCACoordinators_TestApp.swift
+//  TCACoordinators_Test
+//
+//  Created by MarkCloud on 2022/11/23.
+//
+
+import SwiftUI
+
+@main
+struct TCACoordinators_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
